ed341ef4b4cfd8344c2af6220273fac713bf6459
Robert Hall <langbert@gmail.com>
2020-05-17 19:27:59 -0600

    Actual groups

--- api/admin/assets/page-quickarrange.post.js
+++ api/admin/assets/page-quickarrange.post.js
@@ -2,25 +2,33 @@ const { cms_query } = require('../../../loaders.js')
 
 module.exports = async function() {
 	const query = `{
-		ungrouped_assets: assets(where: {
-			AND: [
-				{ archivalAsset: true }
-				{ relatedAssetGroups_every: { id: null} }
-			]
-		}) {
+
+		ungrouped_assets: assets(
+			where: {
+				AND: [
+					{ archivalAsset: true }
+					{ relatedAssetGroups_every: { id: null} }
+				]
+			}
+			orderBy: fileName_ASC
+		) {
 			id
 			handle
 			filename: fileName
 			mime_type: mimeType
 		}
-		grouped_assets: assets(where: {
-			relatedAssetGroups_some: { id_not: null}
-		}) {
+
+		asset_groups: assetGroups(orderBy: title_ASC) {
 			id
-			handle
-			filename: fileName
-			mime_type: mimeType
+			title
+			assets {
+				id
+				handle
+				filename: fileName
+				mime_type: mimeType
+			}
 		}
+
 	}`
 	return await cms_query(query)
 }
