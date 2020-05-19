b4797e3c1bdc0f8abde5785ae4b6a698f5fee3e5
Robert Hall <langbert@gmail.com>
2020-05-19 15:31:29 -0600

    Quick entry data table

--- api/admin/assets/quickentry-page.post.js
+++ api/admin/assets/quickentry-page.post.js
@@ -1,37 +1,49 @@
 const { cms_query } = require('../../../loaders.js')
 
 module.exports = async function() {
+
 	const query = `{
-		assets {
+
+		# assets {
+		# 	id
+		# 	url
+		# 	summary
+		# 	handle
+		# 	filename: fileName
+		# }
+
+		asset_groups: assetGroups(orderBy: title_ASC) {
 			id
-			url
+			title
+			subject
+			content_type: contentType
+			source
 			summary
-			handle
-			filename: fileName
+			# detail {
+			#	 html
+			# }
+			year
+			tags { name: tag }
+			assets { id handle filename: fileName mime_type: mimeType }
 		}
+
+		content_types: __type(name: "ContentTypes") {
+			values: enumValues { value: name }
+		}
+		subjects: __type(name: "Subjects") {
+			values: enumValues { value: name }
+		}
+
 	}`
-	const { assets } = await cms_query(query)
-	return { items: assets }
+
+	const res = await cms_query(query)
+	res.content_types = res.content_types.values.map(value => value.value)
+	res.subjects = res.subjects.values.map(value => value.value)
+	res.asset_groups = res.asset_groups.map(group => {
+		group.tags = group.tags.map(tag => tag.name)
+		return group
+	})
+
+	return res
+
 }
-// const query = `{
-// 	assets(where: { relatedAssetGroups_some: { id_not: null } }) {
-// 		id
-
-// 		url
-// 		summary
-// 		handle
-// 		filename: fileName
-
-// 		relatedAssetGroups {
-// 			id
-// 			title
-// 			subject
-// 			contentType
-// 			source
-// 			summary
-// 			year
-// 			tags { id tag }
-// 			# assets { id }
-// 		}
-// 	}
-// }`
