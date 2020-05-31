9967c52b7ca72df722ebaaf2bfd56dfbbc23e549
Robert Hall <langbert@gmail.com>
2020-05-31 15:15:18 -0600

    Need to fix related—this is not a working solution
    SEE: https://trello.com/c/MrK8Dq9k/162-archive-assets-figure-out-what-constitutes-related

---
+++ api/public/assets/related.post.js
@@ -0,0 +1,31 @@
+const { cms_query } = require('../../../loaders.js')
+
+module.exports = async function() {
+
+	// FIXME: figure out what constitutes "related"
+	// FIXME: figure out what constitutes "related"
+	// FIXME: figure out what constitutes "related"
+
+	return await cms_query(`{
+		related: assetGroups(
+			first: 4
+			orderBy: title_ASC
+		) {
+			id
+			title
+			assets {
+				cover { url summary handle mime_type: mimeType filename: fileName }
+				id url summary handle mime_type: mimeType filename: fileName
+			}
+			asset_links: assetLinks {
+				summary
+				link
+				cover { url handle }
+			}
+			content_type: contentType
+			year
+			subject
+		}
+	}`)
+
+}
