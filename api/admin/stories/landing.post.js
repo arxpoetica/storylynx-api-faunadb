8834784a0a6fd0392ed4ce9d6f5df3d6198dbfde
Robert Hall <langbert@gmail.com>
2020-07-09 18:20:59 -0600

    Images for story thumbs

--- api/admin/stories/landing.post.js
+++ api/admin/stories/landing.post.js
@@ -1,7 +1,7 @@
 const { cms_query } = require('../../../loaders.js')
 
 module.exports = async function() {
-	return await cms_query(`query {
+	const res = await cms_query(`query {
 		stories(orderBy: title_ASC) {
 			id
 			title
@@ -10,8 +10,31 @@ module.exports = async function() {
 				id
 				slug
 				order
+				clips {
+					assetBins {
+						assets(where: { mimeType_contains: "image" }, first: 1) {
+							mimeType
+							handle
+						}
+					}
+				}
 			}
 		}
 	}`)
 
+	res.stories = res.stories.map(story => {
+		story.sequences = story.sequences.map(seq => {
+			const clip = seq.clips.find(clip => {
+				return clip.assetBins.find(bin => {
+					return bin.assets.find(asset => asset.mimeType.includes('image'))
+				})
+			})
+			seq.asset = clip ? clip.assetBins[0].assets[0] : null
+			delete seq.clips
+			return seq
+		})
+		return story
+	})
+
+	return res
 }
