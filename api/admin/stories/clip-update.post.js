7790c69ce27ff9670c4128525fd08556f7eb661c
Robert Hall <langbert@gmail.com>
2020-09-11 12:49:02 -0600

    Update / save (upsert) story clips

---
+++ api/admin/stories/clip-update.post.js
@@ -0,0 +1,118 @@
+const { EOL } = require('os')
+const { cms_mutate } = require('../../../loaders.js')
+
+module.exports = async function({ clip }) {
+
+	let mutation_var_defs = clip.asset_bins.map((bin, bin_i) => {
+		return bin.html_blocks.map((_, block_j) => {
+			return `
+				$name${bin_i}_${block_j}: String
+				$code${bin_i}_${block_j}: String
+				$html${bin_i}_${block_j}: String
+			`
+		})
+	}).filter(Boolean).join(EOL)
+	mutation_var_defs = mutation_var_defs.length ? EOL + mutation_var_defs : ''
+
+
+	const styles_mutation = () => {
+		const { styles } = clip
+		let output = ''
+		if (styles) {
+			output = 'styles: { upsert: {' + EOL
+			output += ['update', 'create'].map(type => {
+				return [
+					`${type}: {`,
+					typeof styles.top === 'number' ? `top: ${styles.top}` : null,
+					typeof styles.right === 'number' ? `right: ${styles.right}` : null,
+					typeof styles.bottom === 'number' ? `bottom: ${styles.bottom}` : null,
+					typeof styles.left === 'number' ? `left: ${styles.left}` : null,
+					typeof styles.width === 'number' ? `templateWidth: ${styles.width}` : null,
+					typeof styles.height === 'number' ? `templateHeight: ${styles.height}` : null,
+					typeof styles.percent === 'boolean' ? `widthAsPercent: ${styles.percent}` : null,
+					typeof styles.gap === 'number' ? `gap: ${styles.gap}` : null,
+					'}',
+				].filter(Boolean).join(EOL)
+			}).join(EOL) + EOL
+			output += '} }'
+		}
+		return output
+	}
+
+	const mutation = `
+		mutation update_clip(
+			$slug: String
+			${mutation_var_defs}
+		) {
+			updated_clip: updateClip(where: { id: "${clip.id}" }, data: {
+
+				# status: PUBLISHED
+				slug: $slug
+				order: "${clip.order}"
+				template: ${clip.template}
+				hideNavigation: ${clip.hide_navigation}
+				themeElements: { set: [${clip.theme_elements.join(', ')}] }
+				transitions: { set: [${clip.transitions.join(', ')}] }
+				${styles_mutation()}
+				assetBins: {
+					upsert: [${clip.asset_bins.map((bin, bin_i) => `{
+						where: { id: "${bin.id}" }
+						create: {
+							order: ${bin.order || 0}
+							${bin.transition ? `transition: ${bin.transition}` : ''}
+							# links: { set: {} }
+							assets: { connect: [${bin.assets.map(asset => `{ id: "${asset.id}" }`).join(' ')}] }
+							htmlBlocks: {
+								create: [${bin.html_blocks.map((block, block_j) => `{
+									name: $name${bin_i}_${block_j}
+									code: $code${bin_i}_${block_j}
+									html: $html${bin_i}_${block_j}
+									${block.template ? `template: ${block.template}` : ''}
+									${block.color ? `highlightColor: ${block.color}` : ''}
+									order: ${block.order || 0}
+								}`).join(EOL)}]
+							}
+						}
+						update: {
+							order: ${bin.order || 0}
+							${bin.transition ? `transition: ${bin.transition}` : ''}
+							# links: { set: {} }
+							assets: { set: [${bin.assets.map(asset => `{ id: "${asset.id}" }`).join(' ')}] }
+							htmlBlocks: {
+								upsert: [${bin.html_blocks.map((block, block_j) => `{
+									where: { id: "${block.id}" }
+									${['create', 'update'].map(type => `${type}: {
+										name: $name${bin_i}_${block_j}
+										code: $code${bin_i}_${block_j}
+										html: $html${bin_i}_${block_j}
+										${block.template ? `template: ${block.template}` : ''}
+										${block.color ? `highlightColor: ${block.color}` : ''}
+										order: ${block.order || 0}
+									}`).join(EOL)}
+								}`).join(EOL)}]
+							}
+						}
+					}`).join(EOL)}]
+				}
+				# um...not letting it be moved around to a different parent sequence yet
+				# parentSequence: { connect: { id: "...parent_id..." } }
+				# parentAudioClips:
+			}) {
+				id
+			}
+		}
+	`
+
+	const variables = { slug: clip.slug || '' }
+	clip.asset_bins.forEach((bin, bin_i) => {
+		bin.html_blocks.forEach((block, block_j) => {
+			variables[`name${bin_i}_${block_j}`] = block.name || ''
+			variables[`code${bin_i}_${block_j}`] = JSON.stringify(block.code || {})
+			variables[`html${bin_i}_${block_j}`] = block.html || ''
+		})
+	})
+
+	const res = await cms_mutate(mutation, variables)
+	return res
+
+}
