404d0e0c6118ae2b84f80f79dff6928bed555176
Robert Hall <langbert@gmail.com>
2020-09-19 00:10:29 -0600

    Clip reordering

---
+++ api/admin/stories/clips-reorder.post.js
@@ -0,0 +1,14 @@
+const { EOL } = require('os')
+const { cms_mutate } = require('../../../loaders.js')
+
+module.exports = async function({ clip_changes }) {
+
+	const mutation = `mutation {
+		${clip_changes.map(({ id, order }) => `
+			clip_${id}: updateClip(where: { id: "${id}" } data: { order: "${order}" }) { id order }
+		`).join(EOL)}
+	}`
+
+	return await cms_mutate(mutation)
+
+}
