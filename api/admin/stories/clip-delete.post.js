dd2f2df3ed995da5ec6f4131e66098a0275f980a
Robert Hall <langbert@gmail.com>
2020-10-03 19:00:53 -0600

    Fixed clip deletion

--- api/admin/stories/clip-delete.post.js
+++ api/admin/stories/clip-delete.post.js
@@ -10,7 +10,7 @@ module.exports = async function({ clip_id, style_id, asset_bin_ids, asset_ids })
 		const joined_asset_bin_ids = asset_bin_ids.map(id => `"${id}"`).join(', ')
 		asset_bins_delete = `deleted_assets_bins: deleteManyAssetsBinsConnection(where: { id_in: [${joined_asset_bin_ids}] }) { aggregate { count } }`
 	}
-	let assets_delete
+	let assets_delete = ''
 	if (asset_ids.length) {
 		const joined_asset_ids = asset_ids.map(id => `"${id}"`).join(', ')
 		assets_delete = `deleted_assets: deleteManyStoryAssetsConnection(where: { id_in: [${joined_asset_ids}] }) { aggregate { count } }`
