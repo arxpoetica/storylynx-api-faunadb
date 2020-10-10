5c7a7ad59bf40debaa2b35527ede9c388a894e0b
Robert Hall <langbert@gmail.com>
2020-10-10 16:36:03 -0600

    NO GOOD…to come later

---
+++ api/admin/uppy/assets-upload.post.js
@@ -0,0 +1,10 @@
+// const { cms_mutate } = require('../../../loaders.js')
+
+module.exports = async function(what, is, it, about) {
+
+	debugger
+	console.log(what, is, it, about)
+
+	return {}
+
+}
