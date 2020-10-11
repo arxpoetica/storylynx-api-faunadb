4192b2f77515b09b83f0e774546313257277055a
Robert Hall <langbert@gmail.com>
2020-10-11 15:34:28 -0600

    Delete bin

---
+++ api/admin/stories/bin-delete.post.js
@@ -0,0 +1,23 @@
+const { cms_mutate } = require('../../../loaders.js')
+
+/* eslint-disable max-len */
+module.exports = async function({ bin_id, asset_ids }) {
+
+
+	let assets_delete = ''
+	if (asset_ids.length) {
+		const joined_asset_ids = asset_ids.map(id => `"${id}"`).join(', ')
+		assets_delete = `deleted_assets: deleteManyStoryAssetsConnection(where: { id_in: [${joined_asset_ids}] }) { aggregate { count } }`
+	}
+
+	const mutation = `
+		mutation {
+			deleted_bin: deleteAssetsBin(where: { id: "${bin_id}" }) { id }
+			${assets_delete}
+		}
+	`
+
+	const res = await cms_mutate(mutation)
+	return res
+
+}
